73793cf14cfb666366bf5191e8859754a16151b3
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-05 15:05:21 +0000

    Match homepage to target

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
-class HomeController < ApplicationController{
+class HomeController < ApplicationController
   def home
+    matching_boards = Board.all
+
+    @list_of_boards = matching_boards.order({ :created_at => :desc })
+
     render({ :template => "boards/index" })
   end
-}
+end
